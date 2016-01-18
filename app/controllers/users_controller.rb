83bb05b0db423d068985a22619e2558afd19e31e
Danny Kim <dkim7474@gmail.com>
2016-01-18 13:37:20 -0500

    Completed CRUD for users model/controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,44 @@
 class UsersController < ApplicationController
+  def index
+  	@users = User.all
+  end	
+
   def show
+  	@user = User.find(params[:id])
   end
 
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to users_path
+  	else
+  		redirect_to new_users_path
+  	end
   end
 
   def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+  	@user.save
+  	redirect_to user_path
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to users_path
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'home/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/edit'
+  # get 'home/index'
 
   root 'home#index'
+
+  resources :users
+# Prefix Verb   URI Pattern               Controller#Action
+#      root GET    /                         home#index
+#     users GET    /users(.:format)          users#index
+#           POST   /users(.:format)          users#create
+#  new_user GET    /users/new(.:format)      users#new
+# edit_user GET    /users/:id/edit(.:format) users#edit
+#      user GET    /users/:id(.:format)      users#show
+#           PATCH  /users/:id(.:format)      users#update
+#           PUT    /users/:id(.:format)      users#update
+#           DELETE /users/:id(.:format)      users#destroy
 end
